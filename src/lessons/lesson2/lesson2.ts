8484c5681be8a7be321dd215aa81926e6f87ffaa
vlad <vnelipovich63@gmail.com>
2023-09-01 12:49:25 +0300

    did task 5.4 in lesson2

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -180,7 +180,41 @@ console.log(fib(77))
 
 //task 5.4
 
+type objType = {
+    value: number;
+    next: objType | null;
+};
 
+function printList(list: objType | null): void {
+    while (list) {
+        console.log(list.value);
+        list = list.next;
+    }
+}
+
+const list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+function printListRecursion(list: objType): void {
+    console.log(list.value)
+    if(list.next){
+        printListRecursion(list.next)
+    }
+}
+
+printListRecursion(list)
+printList(list);
 
 
 // Task 06
