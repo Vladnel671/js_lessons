e117965985cedeaa99b7ee5edfa9f8ef3fd71293
vlad <vnelipovich63@gmail.com>
2023-09-28 22:22:13 +0300

    did task 5 in lesson 4

--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './lesson_4';
+import {handlePromise} from "./lesson_4";
 
 const Lesson4 = () => {
+    const firstHandler = () => {
+        handlePromise.promise = new Promise((resolve, reject) => {
+            handlePromise.resolve = resolve
+            handlePromise.reject = reject
+        })
+    }
+
+    const secondHandler = () => {
+        if (handlePromise.resolve) {
+            handlePromise.onSuccess('resolved data')
+        }
+    }
+
+    const thirdHandler = () => {
+        if (handlePromise.reject) {
+            handlePromise.onError('error message')
+        }
+    }
     return (
         <div>
-            <button id='btn-create-promise'>Create Promise</button>
-            <button id='btn-resolve-promise'>Resolve Promise</button>
-            <button id='btn-reject-promise'>Reject Promise</button>
+            <button id='btn-create-promise' onClick={firstHandler}>Create Promise</button>
+            <button id='btn-resolve-promise' onClick={secondHandler}>Resolve Promise</button>
+            <button id='btn-reject-promise' onClick={thirdHandler}>Reject Promise</button>
         </div>
     );
 }
--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,5 +1,3 @@
-console.log('lesson 4');
-
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 // https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
 
@@ -69,6 +67,27 @@ p4.then(data => {
 // описаного выше объекта: свойство promise получает новый созданный промис,
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
+
+interface IHandlePromise {
+    promise: any | null;
+    resolve: any | null;
+    reject: any | null;
+    onSuccess: (paramName: any) => void;
+    onError: (paramName: any) => void;
+}
+
+
+export const handlePromise: IHandlePromise = {
+    promise: null,
+    resolve: null,
+    reject: null,
+    onSuccess: function (paramName: any) {
+        console.log(`Promise is resolved with data: ${paramName}`)
+    },
+    onError: function (paramName2: any) {
+        console.log(`Promise is rejected with error: ${paramName2}`)
+    }
+}
 
 
 // Task 06
